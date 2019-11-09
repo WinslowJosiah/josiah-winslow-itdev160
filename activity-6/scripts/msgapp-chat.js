1eeddda6163d27a874625b52a6a464834872e4d3
WinslowJosiah <winslowjosiah@gmail.com>
2019-11-09 03:07:52 -0600

    Added simple Connect 4 implementation

---
+++ activity-6/scripts/msgapp-chat.js
@@ -0,0 +1,335 @@
+// Declarations
+
+var chatbot;
+const chatState = {
+	c4Diff: "connect4-setDifficulty",
+	c4Game: "connect4-game",
+	c4GameOver: "connect4-gameOver",
+};
+
+const EMOJI = {
+	COLLISION_SYMBOL: "\uD83D\uDCA5",
+	CROSS_MARK: "\u274C",
+	DOWN_POINTING_RED_TRIANGLE: "\uD83D\uDD3B",
+	HANDSHAKE: "\uD83E\uDD1D",
+	INPUT_SYMBOL_FOR_NUMBERS: "\uD83D\uDD22",
+	KEYCAP: "\uFE0F\u20E3",
+	LARGE_RED_CIRCLE: "\uD83D\uDD34",
+	LARGE_YELLOW_CIRCLE: "\uD83D\uDFE1",
+	SMILING_FACE_WITH_OPEN_MOUTH_AND_SMILING_EYES: "\uD83D\uDE04",
+	WHITE_MEDIUM_SQUARE: "\u25FB",
+	WINKING_FACE: "\uD83D\uDE09"
+}
+
+// Initialize the chat
+function chatInit() {
+	chatbot = {
+		state: undefined,
+		active: true,
+		name: "C.H.A.T.B.O.T.",
+		messages: [],
+		con4: {
+			COLS: 7,
+			ROWS: 6,
+			diff: undefined,
+			board: undefined,
+			lastMove: undefined,
+			cpuTurn: undefined,
+			firstEmptyRow: function(col) {
+				var row = -1;
+				while (this.board[row + 1][col] == 0) {
+					row++;
+					if (row + 1 >= this.ROWS) break;
+				}
+				return row;
+			},
+			makeMove: function(col) {
+				if (col >= 0 && col < this.COLS)
+				{
+					var row = this.firstEmptyRow(col);
+					if (row >= 0)
+					{
+						this.board[row][col] = this.cpuTurn ? 1 : 2;
+						this.lastMove = col;
+						return true;
+					}
+				}
+				return false;
+			},
+			winner: function() {
+				var lastRow = (this.firstEmptyRow(this.lastMove) + 1);
+				var lastCol = this.lastMove;
+				var lastPlayer = this.board[lastRow][lastCol];
+				
+				function checkDir(dir, explode) {
+					var total = 0;
+					
+					var i = lastRow + dir.i;
+					var j = lastCol + dir.j;
+					while (i >= 0 && i < this.ROWS && j >= 0 && j < this.COLS && this.board[i][j]==lastPlayer) {
+						// If you won, turn the winning pieces into explosions
+						if (explode) this.board[i][j] = 3;
+						total++;
+						i += dir.i;
+						j += dir.j;
+					}
+					return total;
+				}
+				
+				function checkFour(dirA, dirB) {
+					var fourInARow = (1 + checkDir.call(this, dirA) + checkDir.call(this, dirB) >= 4);
+					// If you won...
+					if (fourInARow)
+					{
+						// Turn the winning pieces into explosions
+						checkDir.call(this, dirA, true);
+						checkDir.call(this, dirB, true);
+						this.board[lastRow][lastCol] = 3;
+					}
+					return fourInARow;
+				}
+				
+				var dirsToCheck = [
+					[{i: 0, j: -1}, {i: 0, j: 1}], // Horizontal
+					[{i: -1, j: 0}, {i: 1, j: 0}], // Vertical
+					[{i: 1, j: -1}, {i: -1, j: 1}], // Diagonal (/)
+					[{i: 1, j: 1}, {i: -1, j: -1}] // Diagonal (\)
+				];
+				
+				for (var dirs of dirsToCheck)
+				{
+					if (checkFour.apply(this, dirs)) return lastPlayer;
+				}
+				
+				return false;
+			},
+			boardToString: function() {
+				var boardRows = [];
+				
+				for (var i = 0; i < this.ROWS; i++)
+				{
+					var boardRow = "";
+					this.board[i].forEach(function(stone) {
+						boardRow += [EMOJI.WHITE_MEDIUM_SQUARE, EMOJI.LARGE_RED_CIRCLE, EMOJI.LARGE_YELLOW_CIRCLE, EMOJI.COLLISION_SYMBOL][stone];
+					});
+					boardRows.push(boardRow);
+				}
+				
+				return boardRows.join("\n");
+			},
+			movementToString: function(col) {
+				return [...range(0,6)].map(n => n==col ? EMOJI.DOWN_POINTING_RED_TRIANGLE : (this.firstEmptyRow(n) < 0 ? EMOJI.CROSS_MARK : (n+1).toString() + EMOJI.KEYCAP)).join("");
+			},
+			validColsToString: function() {
+				return [...range(0,6)].map(n => this.firstEmptyRow(n) < 0 ? EMOJI.CROSS_MARK : (n+1).toString() + EMOJI.KEYCAP).join("");
+			}
+		}
+	};
+};
+
+// Execute the logic behind the chat
+async function runChat() {
+	var con4 = chatbot.con4;
+	
+	while (true) {
+		con4.state = chatState.c4Diff;
+		con4.diff = undefined;
+		// Loop until a difficulty setting has been chosen
+		do {
+			await sendMsg(`Choose your difficulty...
+1${EMOJI.KEYCAP}: Easy
+2${EMOJI.KEYCAP}: Medium
+3${EMOJI.KEYCAP}: Hard`);
+			
+			chatbot.active = false;
+			await until(() => chatbot.active);
+			
+			processMessages(function(msg, index){
+				if (isUndefined(con4.diff))
+				{
+					if (msg.text.match(/^\s*(?:1|easy)\s*$/i))
+					{
+						con4.diff = 0;
+					}
+					else if (msg.text.match(/^\s*(?:2|med(ium)?)\s*$/i))
+					{
+						con4.diff = 1;
+					}
+					else if (msg.text.match(/^\s*(?:3|hard)\s*$/i))
+					{
+						con4.diff = 2;
+					}
+				}
+			});
+			
+			if (isUndefined(con4.diff))
+			{
+				await sendMsg(`Whoops! That's not a valid difficulty setting!`);
+			}
+		} while (isUndefined(con4.diff));
+		
+		await sendMsg(`You chose ${["Easy", "Medium", "Hard"][con4.diff]}. This is gonna be fun...`);
+		
+		// Populate Connect 4 board
+		con4.board = [];
+		for (var i = 0; i < con4.ROWS; i++)
+		{
+			con4.board[i] = [];
+			for (var j = 0; j < con4.COLS; j++)
+			{
+				con4.board[i][j] = 0;
+			}
+		}
+		
+		// Define initial variables
+		con4.cpuTurn = false;
+		con4.lastMove = undefined;
+		
+		// Play Connect 4 game
+		chatbot.state = chatState.c4Game;
+		await sendMsg(`Here is the game board.
+
+${con4.validColsToString()}
+${con4.boardToString()}`);
+		do {
+			// Allow the next move to be made
+			await makeMove(con4.cpuTurn);
+			// If someone won, stop playing
+			if (con4.winner()) break;
+			con4.cpuTurn = !con4.cpuTurn;
+		} while (true);
+		
+		// Tell us who won
+		chatbot.state = chatState.c4GameOver;
+		if (con4.cpuTurn)
+		{
+			await sendMsg(`Looks like I won! ${EMOJI.SMILING_FACE_WITH_OPEN_MOUTH_AND_SMILING_EYES}
+
+${con4.boardToString()}`);
+		}
+		else
+		{
+			await sendMsg(`Oh my, this is embarrassing...`);
+			await sendMsg(`...I guess you won fair and square, my friend. ${EMOJI.HANDSHAKE} Good game.
+
+${con4.boardToString()}`);
+		}
+		await sendMsg(`I'll reset the pieces, so we can start a new game! ${EMOJI.WINKING_FACE}`);
+	}
+}
+
+// Connect 4 functions
+
+async function makeMove(bot) {
+	var con4 = chatbot.con4;
+	var moveToMake;
+	
+	if (bot)
+	{
+		await sendMsg(`My turn!`);
+		
+		// Choose a good move
+		// (At this point, it's just random)
+		var validMoves = [];
+		for (var i = 0; i < con4.COLS; i++)
+		{
+			if (con4.firstEmptyRow(i) >= 0)
+			{
+				validMoves.push(i);
+			}
+		}
+		
+		moveToMake = validMoves.random();
+		
+		// Make the chosen move
+		con4.makeMove(moveToMake);
+		await sendMsg(`I think I should go here:
+
+${con4.movementToString(moveToMake)}
+${con4.boardToString()}`);
+	}
+	else
+	{
+		// Ask for user input
+		do {
+			sendMsg(`Enter a column number from 1 to 7.`);
+			chatbot.active = false;
+			await until(() => chatbot.active);
+			
+			moveToMake = undefined;
+			processMessages(function(msg, index) {
+				var colMatch = msg.text.match(/^\s*(?:col(?:umn)?\s+)?(\d)\s*$/i);
+				if (colMatch)
+				{
+					moveToMake = Number(colMatch[1]) - 1;
+				}
+			});
+			
+			var moveIsDefined = !isUndefined(moveToMake);
+			var moveIsInBounds = moveToMake >= 0 && moveToMake < con4.COLS;
+			var moveIsValid = con4.firstEmptyRow(moveToMake) >= 0;
+			if (!moveIsDefined)
+			{
+				await sendMsg(`You must enter in a number.`);
+				await sendMsg(`Here's the board again. There are numbers here for a reason...I expect you to use them! ${EMOJI.INPUT_SYMBOL_FOR_NUMBERS}
+
+${con4.validColsToString()}
+${con4.boardToString()}`);
+			}
+			else if (!moveIsInBounds)
+			{
+				await sendMsg(`That's not a valid column number.`);
+				await sendMsg(`Here's the board again. You can make a move in any column from 1 to 7 that doesn't have a ${EMOJI.CROSS_MARK} over it.
+
+${con4.validColsToString()}
+${con4.boardToString()}`);
+			}
+			else if (!moveIsValid)
+			{
+				await sendMsg(`You can't make a move in that column.`);
+				await sendMsg(`Here's the board again, so you know which moves are valid. If you see a ${EMOJI.CROSS_MARK} over a column, you can't make a move there.
+
+${con4.validColsToString()}
+${con4.boardToString()}`);
+			}
+		} while (!(moveIsDefined && moveIsInBounds && moveIsValid));
+		
+		// Make the chosen move
+		con4.makeMove(moveToMake);
+		
+		await sendMsg(`Got it, here's how the board looks now.
+
+${con4.movementToString(moveToMake)}
+${con4.boardToString()}`);
+	}
+}
+
+// Chatbot functions
+
+// Send a message from the chatbot
+async function sendMsg(text) {
+	// Display the typing animation
+	await sleep(500);
+	showTyping();
+	// Simulate typing
+	await sleep(text.length * 20);
+	// Hide the typing animation
+	hideTyping();
+	// Do the rather immediate work of adding the messages
+	appendMessage(messageType.in, chatbot.name, text);
+	// Discard all of the messages sent in that time
+	processMessages(() => null);
+}
+
+// Run a function on every unprocessed message
+function processMessages(callback) {
+	var index = 0;
+	var breakLoop = false;
+	while (chatbot.messages.length > 0)
+	{
+		breakLoop = callback(chatbot.messages[0], index);
+		chatbot.messages.shift();
+		index++;
+		if (breakLoop) break;
+	}
+}
--- activity-6/scripts/msgapp.js
+++ activity-6/scripts/msgapp.js
@@ -4,9 +4,70 @@
 const isUndefined = v => typeof v === "undefined";
 
 // Check if string is either undefined or full of whitespace
-String.prototype.isNullOrWhitespace = function() {
-	if (isUndefined(this) || this == null) return true;
-	return this.replace(/\s/g, "").length < 1;
+if (!String.prototype.isNullOrWhitespace)
+{
+	String.prototype.isNullOrWhitespace = function() {
+		if (isUndefined(this) || this == null) return true;
+		return this.replace(/\s/g, "").length < 1;
+	}
+}
+
+// Return the last element in an array
+if (!Array.prototype.last)
+{
+	Array.prototype.last = function() {
+		return this[this.length - 1];
+	}
+}
+
+// Return the last element in a NodeList
+if (!NodeList.prototype.last)
+{
+	NodeList.prototype.last = function() {
+		return this[this.length - 1];
+	}
+}
+
+// Return a random element from an Array
+if (!Array.prototype.random)
+{
+	Array.prototype.random = function() {
+		return this[Math.floor(Math.random()*this.length)];
+	}
+}
+
+// Generate range of numbers
+const range = function*(start, end, step) {
+	// range(n) is the same as range(0,n)
+	if (isUndefined(end))
+	{
+		end = start;
+		start = 0;
+	}
+	// Default step of 1 or -1
+	if (isUndefined(step)) step = start < end ? 1 : -1;
+	for (var i = start; step > 0 ? i <= end : i >= end; i += step)
+	{
+		yield i;
+	}
+}
+
+// Sleep for a specified number of ms
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+// Wait for condition to be true
+const until = function(condition) {
+	const poll = resolve => {
+		if (condition())
+		{
+			resolve();
+		}
+		else
+		{
+			setTimeout(() => poll(resolve), 100);
+		}
+	}
+	return new Promise(poll);
 }
 
 // Now the REAL code begins
@@ -19,19 +80,9 @@ var messageType = {
 };
 var initialMessages = [
 	{
-		type: messageType.out,
-		user: "Mike",
-		message: "Hey, do you have lunch plans?"
-	},
-	{
 		type: messageType.in,
-		user: "Joe",
-		message: "Hi Mike! No, how about QDoba? 🍔"
-	},
-	{
-		type: messageType.out,
-		user: "Mike",
-		message: "Ok, let's go 😋🍴 I'll meet you there"
+		user: "C.H.A.T.B.O.T.",
+		text: "Hello! Why don't we play a game of Connect 4 together?"
 	}
 ];
 
@@ -43,21 +94,30 @@ window.onload = function() {
 	var sendButtonEl = document.getElementById("send-button");
 	var replyButtonEl = document.getElementById("reply-button");
 	
+	// Libraries are nice
 	autosize(messageInputEl);
 	
+	// Append the initial messages to the DOM
 	messages.forEach(function(msg) {
 		appendMessageObject(msg);
 	});
 	
+	// Run the chatbot
+	chatInit();
+	runChat();
+	
+	// Add event handlers
 	sendButtonEl.onclick = addMessageHandler;
 	replyButtonEl.onclick = addMessageHandler;
 	messageInputEl.onkeydown = function(e) {
 		// If ENTER pressed and not Shift pressed...
-		if ((e.keyCode == 13) && !(e.shiftKey)) {
+		if ((e.keyCode == 13) && !(e.shiftKey))
+		{
 			// Stop a newline from being entered
 			e.preventDefault();
 			// If less than a certain length...
-			if (this.value.length <= 2000) {
+			if (this.value.length <= 2000)
+			{
 				// Send the message
 				sendButtonEl.click();
 			}
@@ -66,10 +126,10 @@ window.onload = function() {
 }
 
 // Message constructor
-function Message(type, user, message) {
+function Message(type, user, text) {
 	this.type = type;
 	this.user = user;
-	this.message = message;
+	this.text = text;
 }
 
 // Handle an event that triggers the addition of a message
@@ -78,13 +138,14 @@ function addMessageHandler(event) {
 	var messageInputEl = document.getElementById("message-input");
 	
 	// Determine message type and set message variables accordingly
-	switch (event.target.id) {
+	switch (event.target.id)
+	{
 		case "send-button":
-			user = "Mike";
+			user = "Me";
 			type = messageType.out;
 			break;
 		case "reply-button":
-			user = "Joe";
+			user = "Me";
 			type = messageType.in;
 			break;
 		default:
@@ -94,14 +155,20 @@ function addMessageHandler(event) {
 	}
 	
 	// Create new message object
-	if (!messageInputEl.value.isNullOrWhitespace()) {
+	if (!messageInputEl.value.isNullOrWhitespace())
+	{
 		appendMessage(type, user, messageInputEl.value);
+		// Reset input
+		messageInputEl.value = "";
+		autosize.update(messageInputEl);
+		// Return control to the chatbot
+		if (type == messageType.out) chatbot.active = true;
 	}
 }
 
-// Append message using type, user, and message
-function appendMessage(type, user, message) {
-	var msg = new Message(type, user, message);
+// Append message using type, user, and text
+function appendMessage(type, user, text) {
+	var msg = new Message(type, user, text);
 	appendMessageObject(msg);
 }
 
@@ -109,10 +176,22 @@ function appendMessage(type, user, message) {
 function appendMessageObject(msg) {
 	var messageInputEl = document.getElementById("message-input");
 	var messageContainerEl = document.getElementById("message-container");
+	var typingAnimationEl = document.getElementById("message-typing");
 	
+	// Remove the tail on the last message if it has the same type and user
+	var messageDivs = messageContainerEl.querySelectorAll(".message");
+	if (messageDivs.length > 0)
+	{
+		var lastMessageEl = messageDivs.last();
+		if (lastMessageEl.getAttribute("data-user") == msg.user && lastMessageEl.getAttribute("data-type") == msg.type)
+		{
+			lastMessageEl.removeAttribute("data-tail");
+		}
+	}
 	messages.push(msg);
+	if (msg.type === messageType.out) chatbot.messages.push(msg);
 	// Create text element for the message
-	var messageString = msg.message;
+	var messageString = msg.text;
 	// Create message element and insert the message text
 	var messageEl = document.createElement("div");
 	messageString.split(/\r\n|\r|\n/).forEach(function(line,i,lines) {
@@ -123,11 +202,33 @@ function appendMessageObject(msg) {
 	twemoji.parse(messageEl);
 	// Add classes that make it a message
 	messageEl.classList.add("message");
-	messageEl.classList.add(msg.type);
+	messageEl.setAttribute("data-type", msg.type);
 	messageEl.setAttribute("data-user", msg.user);
+	messageEl.setAttribute("data-tail","");
+	// Hide the typing animation
+	hideTyping();
 	// Add message element to the DOM
-	messageContainerEl.appendChild(messageEl);
-	// Reset input
-	messageInputEl.value = "";
-	autosize.update(messageInputEl);
+	messageContainerEl.insertBefore(messageEl, typingAnimationEl);
+	// Scroll down to the message
+	scrollMessages();
+}
+
+// Scroll the message container all the way to the bottom
+function scrollMessages() {
+	var messageContainerEl = document.getElementById("message-container");
+	messageContainerEl.scrollTop = messageContainerEl.scrollHeight;
+}
+
+// Show typing animation
+function showTyping() {
+	var typingAnimationEl = document.getElementById("message-typing");
+	typingAnimationEl.style.display = "block";
+	scrollMessages();
+}
+
+// Hide typing animation
+function hideTyping() {
+	var typingAnimationEl = document.getElementById("message-typing");
+	typingAnimationEl.style.display = "none";
+	scrollMessages();
 }
