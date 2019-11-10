bd49c924a4eb0958a58a485c37342f70d78738b1
WinslowJosiah <winslowjosiah@gmail.com>
2019-11-10 07:15:35 -0600

    Added Connect 4 AI (it's not good)

--- activity-6/scripts/msgapp-chat.js
+++ activity-6/scripts/msgapp-chat.js
@@ -16,6 +16,7 @@ const EMOJI = {
 	KEYCAP: "\uFE0F\u20E3",
 	LARGE_RED_CIRCLE: "\uD83D\uDD34",
 	LARGE_YELLOW_CIRCLE: "\uD83D\uDFE1",
+	SMILING_CAT_FACE_WITH_OPEN_MOUTH: "\uD83D\uDE3A",
 	SMILING_FACE_WITH_OPEN_MOUTH_AND_SMILING_EYES: "\uD83D\uDE04",
 	WHITE_MEDIUM_SQUARE: "\u25FB",
 	WINKING_FACE: "\uD83D\uDE09"
@@ -35,6 +36,7 @@ function chatInit() {
 			ROWS: 6,
 			diff: undefined,
 			board: undefined,
+			moves: undefined,
 			lastMove: undefined,
 			cpuTurn: undefined,
 			firstEmptyRow: function(col) {
@@ -45,6 +47,9 @@ function chatInit() {
 				}
 				return row;
 			},
+			moveIsValid: function(col) {
+				return this.firstEmptyRow(col) >= 0;
+			},
 			makeMove: function(col) {
 				if (col >= 0 && col < this.COLS)
 				{
@@ -53,12 +58,14 @@ function chatInit() {
 					{
 						this.board[row][col] = this.cpuTurn ? 1 : 2;
 						this.lastMove = col;
+						this.moves++;
+						this.cpuTurn = !this.cpuTurn;
 						return true;
 					}
 				}
 				return false;
 			},
-			winner: function() {
+			determineWinner: function() {
 				var lastRow = (this.firstEmptyRow(this.lastMove) + 1);
 				var lastCol = this.lastMove;
 				var lastPlayer = this.board[lastRow][lastCol];
@@ -103,7 +110,7 @@ function chatInit() {
 					if (checkFour.apply(this, dirs)) return lastPlayer;
 				}
 				
-				return false;
+				return this.moves >= this.ROWS * this.COLS ? undefined : false;
 			},
 			boardToString: function() {
 				var boardRows = [];
@@ -119,11 +126,14 @@ function chatInit() {
 				
 				return boardRows.join("\n");
 			},
-			movementToString: function(col) {
-				return [...range(0,6)].map(n => n==col ? EMOJI.DOWN_POINTING_RED_TRIANGLE : (this.firstEmptyRow(n) < 0 ? EMOJI.CROSS_MARK : (n+1).toString() + EMOJI.KEYCAP)).join("");
+			boardToConsoleString: function() {
+				return this.boardToString().replace(EMOJI.WHITE_MEDIUM_SQUARE,".").replace(EMOJI.LARGE_RED_CIRCLE,"O").replace(EMOJI.LARGE_YELLOW_CIRCLE,"0").replace(EMOJI.COLLISION_SYMBOL,"*");
+			},
+			movementToString: function() {
+				return [...range(0,6)].map(n => n==this.lastMove ? EMOJI.DOWN_POINTING_RED_TRIANGLE : (this.moveIsValid(n) ? (n+1).toString() + EMOJI.KEYCAP : EMOJI.CROSS_MARK)).join("");
 			},
 			validColsToString: function() {
-				return [...range(0,6)].map(n => this.firstEmptyRow(n) < 0 ? EMOJI.CROSS_MARK : (n+1).toString() + EMOJI.KEYCAP).join("");
+				return [...range(0,6)].map(n => this.moveIsValid(n) ? (n+1).toString() + EMOJI.KEYCAP : EMOJI.CROSS_MARK).join("");
 			}
 		}
 	};
@@ -132,7 +142,7 @@ function chatInit() {
 // Execute the logic behind the chat
 async function runChat() {
 	var con4 = chatbot.con4;
-	
+		
 	while (true) {
 		con4.state = chatState.c4Diff;
 		con4.diff = undefined;
@@ -184,8 +194,10 @@ async function runChat() {
 		}
 		
 		// Define initial variables
-		con4.cpuTurn = false;
+		con4.cpuTurn = Math.random() < .5;
 		con4.lastMove = undefined;
+		con4.moves = 0;
+		var winner;
 		
 		// Play Connect 4 game
 		chatbot.state = chatState.c4Game;
@@ -195,53 +207,93 @@ ${con4.validColsToString()}
 ${con4.boardToString()}`);
 		do {
 			// Allow the next move to be made
-			await makeMove(con4.cpuTurn);
-			// If someone won, stop playing
-			if (con4.winner()) break;
-			con4.cpuTurn = !con4.cpuTurn;
-		} while (true);
+			await makeGameMove(con4.cpuTurn);
+			// Check for a winner
+			winner = con4.determineWinner();
+		} while (winner === false);
 		
 		// Tell us who won
 		chatbot.state = chatState.c4GameOver;
-		if (con4.cpuTurn)
-		{
-			await sendMsg(`Looks like I won! ${EMOJI.SMILING_FACE_WITH_OPEN_MOUTH_AND_SMILING_EYES}
+		switch (winner) {
+			// CPU won
+			case 1:
+				await sendMsg(`Looks like I won! ${EMOJI.SMILING_FACE_WITH_OPEN_MOUTH_AND_SMILING_EYES}
 
 ${con4.boardToString()}`);
-		}
-		else
-		{
-			await sendMsg(`Oh my, this is embarrassing...`);
-			await sendMsg(`...I guess you won fair and square, my friend. ${EMOJI.HANDSHAKE} Good game.
+				break;
+			// Human won
+			case 2:
+				await sendMsg(`Oh my, this is embarrassing...`);
+				await sendMsg(`...I guess you won fair and square, my friend. ${EMOJI.HANDSHAKE} Good game.
 
 ${con4.boardToString()}`);
+				break;
+			// 
+			default:
+				await sendMsg(`Hm. This game doesn't have a winner.
+
+That's odd.`);
+				await sendMsg(`Cat game, I guess. ${EMOJI.SMILING_CAT_FACE_WITH_OPEN_MOUTH}
+
+${con4.boardToString()}`);
+				break;
 		}
+		
 		await sendMsg(`I'll reset the pieces, so we can start a new game! ${EMOJI.WINKING_FACE}`);
 	}
 }
 
 // Connect 4 functions
 
-async function makeMove(bot) {
+async function makeGameMove(bot) {
 	var con4 = chatbot.con4;
 	var moveToMake;
 	
 	if (bot)
 	{
-		await sendMsg(`My turn!`);
+		await sendMsg(con4.moves == 0 ? "I'll go first." : "My turn!");
 		
-		// Choose a good move
-		// (At this point, it's just random)
+		// Only choose from the list of valid moves
 		var validMoves = [];
 		for (var i = 0; i < con4.COLS; i++)
 		{
-			if (con4.firstEmptyRow(i) >= 0)
+			if (con4.moveIsValid(i))
 			{
 				validMoves.push(i);
 			}
 		}
 		
-		moveToMake = validMoves.random();
+		// The best moves are the ones that will win
+		var bestMoves = [];
+		var newState;
+		validMoves.forEach(function(move) {
+			newState = _.cloneDeep(con4);
+			newState.makeMove(move);
+			if (newState.determineWinner() == 1) bestMoves.push(move);
+		});
+		
+		// If no moves will win...
+		if (bestMoves.length == 0)
+		{
+			// Calculate which move will lead to a win
+			var moveScores = Array(con4.COLS).fill(-Number.MAX_VALUE);
+			for (var move of validMoves)
+			{
+				newState = _.cloneDeep(con4);
+				newState.makeMove(move);
+				// I wish I could do this for longer without hanging the browser
+				moveScores[move] = -negamax(newState, -6, 6, 3 + (2 * con4.diff));
+			}
+			
+			var maxScore = moveScores.reduce((a,b) => Math.max(a,b));
+			for (var i = 0; i < con4.COLS; i++)
+			{
+				if (moveScores[i] == maxScore) bestMoves.push(i);
+			}
+		}
+		
+		// Choose a random one out of the best moves
+		moveToMake = bestMoves.random();
 		
 		// Make the chosen move
 		con4.makeMove(moveToMake);
@@ -254,7 +306,7 @@ ${con4.boardToString()}`);
 	{
 		// Ask for user input
 		do {
-			sendMsg(`Enter a column number from 1 to 7.`);
+			await sendMsg(`Enter a column number from 1 to 7.`);
 			chatbot.active = false;
 			await until(() => chatbot.active);
 			
@@ -267,10 +319,10 @@ ${con4.boardToString()}`);
 				}
 			});
 			
-			var moveIsDefined = !isUndefined(moveToMake);
-			var moveIsInBounds = moveToMake >= 0 && moveToMake < con4.COLS;
-			var moveIsValid = con4.firstEmptyRow(moveToMake) >= 0;
-			if (!moveIsDefined)
+			var moveDefined = !isUndefined(moveToMake);
+			var moveInBounds = moveToMake >= 0 && moveToMake < con4.COLS;
+			var moveValid = con4.moveIsValid(moveToMake);
+			if (!moveDefined)
 			{
 				await sendMsg(`You must enter in a number.`);
 				await sendMsg(`Here's the board again. There are numbers here for a reason...I expect you to use them! ${EMOJI.INPUT_SYMBOL_FOR_NUMBERS}
@@ -278,7 +330,7 @@ ${con4.boardToString()}`);
 ${con4.validColsToString()}
 ${con4.boardToString()}`);
 			}
-			else if (!moveIsInBounds)
+			else if (!moveInBounds)
 			{
 				await sendMsg(`That's not a valid column number.`);
 				await sendMsg(`Here's the board again. You can make a move in any column from 1 to 7 that doesn't have a ${EMOJI.CROSS_MARK} over it.
@@ -286,7 +338,7 @@ ${con4.boardToString()}`);
 ${con4.validColsToString()}
 ${con4.boardToString()}`);
 			}
-			else if (!moveIsValid)
+			else if (!moveValid)
 			{
 				await sendMsg(`You can't make a move in that column.`);
 				await sendMsg(`Here's the board again, so you know which moves are valid. If you see a ${EMOJI.CROSS_MARK} over a column, you can't make a move there.
@@ -294,7 +346,7 @@ ${con4.boardToString()}`);
 ${con4.validColsToString()}
 ${con4.boardToString()}`);
 			}
-		} while (!(moveIsDefined && moveIsInBounds && moveIsValid));
+		} while (!(moveDefined && moveInBounds && moveValid));
 		
 		// Make the chosen move
 		con4.makeMove(moveToMake);
@@ -302,6 +354,60 @@ ${con4.boardToString()}`);
 		await sendMsg(`${con4.movementToString(moveToMake)}
 ${con4.boardToString()}`);
 	}
+}
+
+// Recursively solve a Connect 4 board
+// (don't ask me how this works)
+function negamax(state, alpha, beta, depth) {
+	var con4 = chatbot.con4;
+	
+	if (isUndefined(state.determineWinner())) return 0;
+	
+	var newState;
+	for (var j = 0; j < con4.COLS; j++)
+	{
+		// I hate that I need to import a whole library for this,
+		// but I can't figure out how to do this basic thing without it
+		newState = _.cloneDeep(state);
+		if (newState.moveIsValid(j))
+		{
+			newState.makeMove(j);
+			if (newState.determineWinner() !== false && !isUndefined(newState.determineWinner()))
+			{
+				return Math.floor((con4.ROWS * con4.COLS + 1 - newState.moves) / 2);
+			}
+		}
+	}
+	
+	var min = -Math.floor((con4.ROWS * con4.COLS - 2 - state.moves) / 2);
+	var max = Math.floor((con4.ROWS * con4.COLS - 1 - state.moves) / 2);
+	if (alpha < min)
+	{
+		alpha = min;
+		if (alpha >= beta) return alpha;
+	}
+	if (beta > max)
+	{
+		beta = max;
+		if (alpha >= beta) return beta;
+	}
+	
+	if (depth == 0) return beta;
+	
+	var score;
+	for (var j = 0; j < con4.COLS; j++)
+	{
+		if (state.moveIsValid(j))
+		{
+			// Ditto
+			newState = _.cloneDeep(state);
+			newState.makeMove(j);
+			score = -negamax(newState, -beta, -alpha, depth - 1);
+			if (score >= beta) return score;
+			if (score > alpha) alpha = score;
+		}
+	}
+	return alpha;
 }
 
 // Chatbot functions
@@ -312,17 +418,19 @@ async function sendMsg(text) {
 	await sleep(500);
 	showTyping();
 	// Simulate typing
-	await sleep(text.replace(emojiRegex, "").length * 15);
+	await sleep(text.replace(emojiRegex, " ").length * 16);
 	// Hide the typing animation
 	hideTyping();
-	// Do the rather immediate work of adding the messages
+	// Adding the message
 	appendMessage(messageType.in, chatbot.name, text);
+	// Wait a while (why do I need to do this)
+	await sleep(10);
 	// Discard all of the messages sent in that time
 	processMessages(() => null);
 }
 
 // Run a function on every unprocessed message
-function processMessages(callback) {
+async function processMessages(callback) {
 	var index = 0;
 	var breakLoop = false;
 	while (chatbot.messages.length > 0)
